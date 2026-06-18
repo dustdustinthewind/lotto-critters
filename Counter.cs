f4033c05690f33028b5296e16f4f87c2ee74820a
dust <dustdustinthewind@proton.me>
2026-06-18 12:30:55 -0700

    Added basic counters

---
+++ Counter.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+// Todo sort machines on counter
+// Better sprite than just a polygon blegh
+
+public partial class Counter : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
