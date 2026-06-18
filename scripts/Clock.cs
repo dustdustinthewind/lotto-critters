fcd950b07c2a7c4b128132274d29505b702a2239
dust <dustdustinthewind@proton.me>
2026-06-18 15:02:57 -0700

    tab indentation changed to 4 spaces
    to match godot

--- scripts/Clock.cs
+++ scripts/Clock.cs
@@ -3,59 +3,59 @@ using System;
 
 public partial class Clock : Node
 {
-  public ulong PlayTimeElapsed
-  {
-	get
-	{
-	  ulong additionalTime = 0;
-	  if (!paused)
-		additionalTime = CurrentEngineTime - lastUnpauseStartTime;
-	  return timeElapsedBeforePause + additionalTime;
-	}
-  }
+    public ulong PlayTimeElapsed
+    {
+        get
+        {
+            ulong additionalTime = 0;
+            if (!paused)
+            additionalTime = CurrentEngineTime - lastUnpauseStartTime;
+            return timeElapsedBeforePause + additionalTime;
+        }
+    }
 
-  public ulong PlayTimeElapsedInSeconds
-  {
-	  get => PlayTimeElapsed / 1000;
-  }
+    public ulong PlayTimeElapsedInSeconds
+    {
+        get => PlayTimeElapsed / 1000;
+    }
 
-  private bool paused = true;
+    private bool paused = true;
 
-  public bool IsPaused {get => paused;}
+    public bool IsPaused {get => paused;}
 
-  private ulong timeElapsedBeforePause = 0;
-  private ulong lastUnpauseStartTime = 0;
+    private ulong timeElapsedBeforePause = 0;
+    private ulong lastUnpauseStartTime = 0;
 
-  public ulong CurrentEngineTime => Time.GetTicksMsec();
+    public ulong CurrentEngineTime => Time.GetTicksMsec();
 
-  // Starts or restarts clock
-  public void RestartClock()
-  {
-	lastUnpauseStartTime = CurrentEngineTime;
-	timeElapsedBeforePause = 0;
-  paused = false;
-  }
+    // Starts or restarts clock
+    public void RestartClock()
+    {
+        lastUnpauseStartTime = CurrentEngineTime;
+        timeElapsedBeforePause = 0;
+        paused = false;
+    }
 
-  public void ResumeClock()
-  {
-	  lastUnpauseStartTime = CurrentEngineTime;
-  	paused = false;
-  }
+    public void ResumeClock()
+    {
+        lastUnpauseStartTime = CurrentEngineTime;
+        paused = false;
+    }
 
-  public void PauseClock()
-  {
-	  timeElapsedBeforePause = timeElapsedBeforePause + CurrentEngineTime - lastUnpauseStartTime;
-	  paused = true;
-  }
+    public void PauseClock()
+    {
+        timeElapsedBeforePause = timeElapsedBeforePause + CurrentEngineTime - lastUnpauseStartTime;
+        paused = true;
+    }
 
-  public string PlayTimeAsString()
-  {
-	TimeSpan t = TimeSpan.FromSeconds(PlayTimeElapsedInSeconds);
-	return string.Format("{0:D2}:{1:D2}:{2:D2}",
-	  t.Hours,
-	  t.Minutes,
-	  t.Seconds);
-  }
+    public string PlayTimeAsString()
+    {
+        TimeSpan t = TimeSpan.FromSeconds(PlayTimeElapsedInSeconds);
+        return string.Format("{0:D2}:{1:D2}:{2:D2}",
+        t.Hours,
+        t.Minutes,
+        t.Seconds);
+    }
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
