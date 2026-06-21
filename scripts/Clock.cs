2b66ac0c57ae6debfaa32e4297b73f530b3c353c
dust <dustdustinthewind@proton.me>
2026-06-21 14:12:16 -0700

    add TimeAsString method to clock

--- scripts/Clock.cs
+++ scripts/Clock.cs
@@ -51,7 +51,12 @@ public partial class Clock : Node
 
 	public string PlayTimeAsString()
 	{
-		TimeSpan t = TimeSpan.FromSeconds(PlayTimeElapsedInSeconds);
+		return TimeAsString(PlayTimeElapsedInSeconds);
+	}
+
+	public string TimeAsString(ulong time)
+	{
+		TimeSpan t = TimeSpan.FromSeconds(time);
 		return string.Format("{0:D2}:{1:D2}:{2:D2}",
 		t.Hours,
 		t.Minutes,
