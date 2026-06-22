9edd88fda9c13b6e7c049a1734e5836c90458524
mxyzptlk-dog <badmache@gmail.com>
2026-06-21 21:18:01 -0700

    added spotlight drop down animation when Parlay Door is clicked
    Slightly bugged! Need to add the reverse animation when the door is toggled off.

---
+++ scenes/animation_player.gd
@@ -0,0 +1,18 @@
+extends AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_parlay_door_toggled(toggled_on: bool, _extra_arg_0: bool) -> void:
+	if toggled_on == true:
+		play("parlay_spotlight_down")
+	else:
+		stop()
